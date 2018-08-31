1267f72a7186e6d6818c008f80fb93e04fdb0e05
Rye-Guy <ryanracioppo95@gmail.com>
2018-08-31 17:36:53 -0400

    seriously it wont work anymore, why?, am I losing it?

--- server.js
+++ server.js
@@ -12,27 +12,11 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-//    //Associated Press Link 
-
-// var options = {
-//     url: 'https://www.apnews.com/tag/apf-topnews',
-//     headers: {
-//       'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4'
-//     }
-//   };
-
-//     request(options, (err, response, html) => {
-//             const $ = cheerio.load(html);
-//             console.log(html, response);
-//             $("p").each((i, element) =>{
-//                     console.log(element);
-//             });
-//     });
-
-
 (async function() {
+    
     const instance = await phantom.create();
-    const page = await instance.createPage();
+    const page = await instance.createPage()
+    //on function you can listen to the events that the page emits. 
     await page.on('onResourceRequested', function(requestData) {
     //   console.info('Requesting', requestData.url);
     });
@@ -43,16 +27,23 @@ app.use(bodyParser.urlencoded({extended: false}));
     // console.log(content);
     
     const $  = cheerio.load(content);
-    
-        $('.contentArticle').each(function(i, element){
+
+        $('.contentArticle').each((i, element) =>{
             // console.l3og(element);
             title = $(this).children('.contentTitle').text();
-            titleText = $(this).children(".firstWords").text();
-            articleLink = $(this).attr("href").val();
+            // titleText = $(this).children(".firstWords").text();
+            // articleLink = $(this).attr("href").val();
             console.log(title);
-            console.log(titleText);
+            // console.log(titleText);
+            // console.log(articleLink);
         });
-   
+
+        // $('.cardContainer').each((i, element) =>{
+        //     title = $(this).children('.ng-scope').children('contentContainer').children('.articleWithoutPicture').children('contentArticle').children('.contentTitle').text();
+            
+        //     console.log(title);
+        // });
+
     await instance.exit();
   })();
 
