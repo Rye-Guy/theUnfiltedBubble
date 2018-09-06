a6da8a21b795e622d0268a3999af359076f6ad39
Rye-Guy <ryanracioppo95@gmail.com>
2018-09-06 16:39:08 -0400

    errors in creating users

--- controllers/router.js
+++ controllers/router.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) =>{
 });
 
 router.post('/', (req, res) =>{
-
+    console.log(req.body);
     //when password does not match when registering
     if(req.body.password !== req.body.confirmPassword){
         let err = new Error('Passwords are not the same');
@@ -17,6 +17,7 @@ router.post('/', (req, res) =>{
     }
     //if all is good after that then take that sweet data and toss it to Mongo
     if(req.body.username && req.body.password){
+        
         let userData = {
             username: req.body.username,
             password: req.body.password
@@ -24,8 +25,9 @@ router.post('/', (req, res) =>{
         //create the user in the database and apply 
         User.create(userData, (err, user) =>{
             if(err){
-                return err;
+                console.log(err);
             }else{
+                console.log(user);
                 req.session.userId = user._id;
                 return res.redirect('index');
             }
@@ -38,7 +40,8 @@ router.post('/', (req, res) =>{
 });
 
 router.get('/home', function(req, res){
-    User.findById(req.session.userId).exec((err, user)=>{
+    User.findById(req.session.userId)
+    .exec((err, user)=>{
         if(err){
             console.log(err);
         }else{
@@ -48,6 +51,7 @@ router.get('/home', function(req, res){
                 return console.log(err);
             }
             else{
+                console.log(user);
                 return res.render('home');
             }
         }
