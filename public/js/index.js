a40ab1b48d8cc50d82de9cf7c38cc4fd74d3924b
Rye-Guy <ryanracioppo95@gmail.com>
2018-09-14 15:59:26 -0400

    articles looking good for now, now just appending to the DOM is the next step

--- public/js/index.js
+++ public/js/index.js
@@ -4,24 +4,31 @@ fetch('/getArticles').then(function(response){
     return response.json()
 }).then(function (articleJSONData){
     console.log(articleJSONData);
-    return articleJSONData; 
-}).then(function createArticles(articleJSONData){
-    let newCard = document.createElement('div');
-    newCard.className = 'card blue-grey darken-1';
-    let articleText = document.createElement('div');
-    articleText.className = 'card-content white-text';
-    let articleTextTitle = document.createElement('span');
-    articleTextTitle.className = 'card-title';
-    let articleTextDescription = document.createElement('p');
     for(i = 0; i < articleJSONData.length; i++){
+        let newCard = document.createElement('div');
+        newCard.className = 'card blue-grey darken-1';
+        let articleText = document.createElement('div');
+        articleText.className = 'card-content white-text';
+        let articleTextTitle = document.createElement('span');
+        articleTextTitle.className = 'card-title';
+        let articleTextDescription = document.createElement('p');
+        let articleLinkArea = document.createElement('div');
+        articleLinkArea.className = 'card-action';
+        let articleLink = document.createElement('a');
+        articleLink.innerText = 'Article Link';
+        let publicationString = document.createElement('a');
+        publicationString.innerText = 'Source: ' + articleJSONData[i].sourcePublication;
         articleTextTitle.innerText = articleJSONData[i].articleTitle;
         articleTextDescription.innerText = articleJSONData[i].articleDescription;
+        articleLink.href = articleJSONData[i].articleUrl;
         newCard.append(articleText);
+        newCard.append(articleLinkArea);
         articleText.append(articleTextTitle);
         articleText.append(articleTextDescription);
+        articleLinkArea.append(articleLink);
+        articleLinkArea.append(publicationString);
         console.log(newCard);
     }
-    
 });
 
 
