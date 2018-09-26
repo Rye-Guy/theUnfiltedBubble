00c40b16f474309bc5ca3aeeb1b6a15c120105ef
Rye-Guy <ryanracioppo95@gmail.com>
2018-09-26 17:55:37 -0400

    same animation added to login

--- public/js/login.js
+++ public/js/login.js
@@ -1,17 +1,29 @@
 loginTabBtn = document.getElementById('loginTab');
 loginForm = document.getElementById('loginForm');
-registerTabBTn = document.getElementById('registerTab');
-regusterForm = document.getElementById('registerForm');
+registerTabBtn = document.getElementById('registerTab');
+registerForm = document.getElementById('registerForm');
 
 
 
 loginTabBtn.addEventListener('click', ()=>{
     console.log('clicked');
     if(loginForm.getAttribute('state') == 'out'){
-        loginForm.style = 'animation-duration: 3s; animation-name: slideIn; animation-fill-mode: forwards;'
+        loginForm.style = 'animation-duration: 1s; animation-name: slideIn; animation-fill-mode: forwards;'
         loginForm.setAttribute('state', 'in') 
     }else if(loginForm.getAttribute('state') == 'in'){
-        loginForm.style = 'animation-duration: 3s; animation-name: slideOut; animation-fill-mode: forwards;'
+        loginForm.style = 'animation-duration: 1.5s; animation-name: slideOut; animation-fill-mode: forwards;'
         loginForm.setAttribute('state', 'out');
     }
 });
+
+registerTabBtn.addEventListener('click', ()=>{
+    console.log('clicked');
+    if(registerForm.getAttribute('state') == 'out'){
+        registerForm.style = 'animation-duration: 1s; animation-name: slideIn; animation-fill-mode: forwards;'
+        registerForm.setAttribute('state', 'in') 
+    }else if(registerForm.getAttribute('state') == 'in'){
+        registerForm.style = 'animation-duration: 1.5s; animation-name: slideOut; animation-fill-mode: forwards;'
+        registerForm.setAttribute('state', 'out');
+    }
+});
+
