9ae93676e00637d52d066ffaac89b86ee5546033
Rye-Guy <ryanracioppo95@gmail.com>
2018-09-27 10:07:35 -0400

    polished animations for login page just need to add a stock image for the login page and maybe a not stock font. oh and my ultimate css button

--- public/js/login.js
+++ public/js/login.js
@@ -6,10 +6,14 @@ registerForm = document.getElementById('registerForm');
 
 
 loginTabBtn.addEventListener('click', ()=>{
-    console.log('clicked');
-    if(loginForm.getAttribute('state') == 'out'){
-        loginForm.style = 'animation-duration: 1s; animation-name: slideIn; animation-fill-mode: forwards;'
-        loginForm.setAttribute('state', 'in') 
+    if(registerForm.getAttribute('state') == 'in' && loginForm.getAttribute('state') == 'out'){
+        registerForm.style = 'animation-duration: 1.5s; animation-name: slideOut; animation-fill-mode: forwards;'
+        registerForm.setAttribute('state', 'out');
+        loginForm.style = 'animation-duration: 1s; animation-name: slideIn; animation-fill-mode: forwards;';
+        loginForm.setAttribute('state', 'in');
+    }else if(loginForm.getAttribute('state') == 'out'){
+        loginForm.style = 'animation-duration: 1s; animation-name: slideIn; animation-fill-mode: forwards;';
+        loginForm.setAttribute('state', 'in');
     }else if(loginForm.getAttribute('state') == 'in'){
         loginForm.style = 'animation-duration: 1.5s; animation-name: slideOut; animation-fill-mode: forwards;'
         loginForm.setAttribute('state', 'out');
@@ -17,8 +21,12 @@ loginTabBtn.addEventListener('click', ()=>{
 });
 
 registerTabBtn.addEventListener('click', ()=>{
-    console.log('clicked');
-    if(registerForm.getAttribute('state') == 'out'){
+    if(loginForm.getAttribute('state') == 'in' && registerForm.getAttribute('state') == 'out'){
+        loginForm.style = 'animation-duration: 1.5s; animation-name: slideOut; animation-fill-mode: forwards;'
+        loginForm.setAttribute('state', 'out');
+        registerForm.style = 'animation-duration: 1s; animation-name: slideIn; animation-fill-mode: forwards;'
+        registerForm.setAttribute('state', 'in');
+    }else if(registerForm.getAttribute('state') == 'out'){
         registerForm.style = 'animation-duration: 1s; animation-name: slideIn; animation-fill-mode: forwards;'
         registerForm.setAttribute('state', 'in') 
     }else if(registerForm.getAttribute('state') == 'in'){
